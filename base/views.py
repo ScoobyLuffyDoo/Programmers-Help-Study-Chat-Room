721ceed72ab5346ed3871095830f37f4bc8c3d90
ScoobyLuffyDoo <ScoobyLuffyDoo@gmail.com>
2022-02-25 01:47:02 +0200

    Chanegd Teme

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from unittest import expectedFailure
+from venv import create
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib import messages
@@ -66,7 +67,7 @@ def home(request):
         Q(description__icontains =q)
         )
     
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()    
     room_count = rooms.count()
     room_messages = Message.objects.filter(
         Q(room__topic__name__icontains =q)
@@ -101,14 +102,18 @@ def userProfile(request,pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST) # uses the model in the form.py
-        if form.is_valid():
-            room =form.save(commit=False)
-            room.host =request.user
-            room.save()
-            return redirect('home')
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic,created =Topic.objects.get_or_create(name=topic_name)
+        Room.objects.create(
+            host=request.user,
+            topic = topic,
+            name = request.POST.get('name'),
+            description =request.POST.get('description'),
+        )       
+        return redirect('home')
+    context = {'form': form,'topics':topics}
     return render(request, 'base/room_form.html',context)
 
 
@@ -118,14 +123,18 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
     if request.user != room.host :
         return HttpResponse('You are not allowed here ')
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)#update the room 
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form':form}
+        topic_name = request.POST.get('topic')
+        topic,created =Topic.objects.get_or_create(name=topic_name)
+        room.topic = topic
+        room.name = request.POST.get('name')        
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
+    context = {'form':form,'topics':topics,'room':room}
     return render(request,'base/room_form.html',context)
 
 @login_required(login_url='login')
